7b1a88f862ec77672ebd34c93cfd60da027f2a84
christian-oreilly <christian.oreilly@epfl.ch>
2016-09-18 14:04:56 +0200

    Adding imports.

--- spinkyDemo/__init__.py
+++ spinkyDemo/__init__.py
@@ -1,5 +1,6 @@
 from .spinkyNbHelperFct import getTrainSignal, getTestSignal, callMatlabFunc, \
     training_process, sp_thresholds_ranges, kp_thresholds_ranges, test_process, \
-    sensitivity, specificity, accuracy, PPV, NPV, MCC, randomAggreProb, cohenk, F1
+    sensitivity, specificity, accuracy, PPV, NPV, MCC, randomAggreProb, cohenk, \
+    F1, readDetectorOutput
 
 
